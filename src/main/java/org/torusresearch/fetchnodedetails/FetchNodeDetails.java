7386b04f83347f7267a9d599839debc76294f621
Gaurav Goel <grvgoel19@gmail.com>
2023-05-23 10:04:57 +0530

    Added sapphire end-points

--- src/main/java/org/torusresearch/fetchnodedetails/FetchNodeDetails.java
+++ src/main/java/org/torusresearch/fetchnodedetails/FetchNodeDetails.java
@@ -1,17 +1,13 @@
 package org.torusresearch.fetchnodedetails;
 
-
 import org.torusresearch.fetchnodedetails.types.NodeDetails;
-import org.torusresearch.fetchnodedetails.types.NodeInfo;
 import org.torusresearch.fetchnodedetails.types.TorusNetwork;
-import org.torusresearch.fetchnodedetails.types.TorusNodePub;
 import org.web3j.crypto.Credentials;
 import org.web3j.crypto.Hash;
 import org.web3j.protocol.Web3j;
 import org.web3j.protocol.http.HttpService;
 import org.web3j.tx.gas.DefaultGasProvider;
 
-import java.math.BigInteger;
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.concurrent.CompletableFuture;
@@ -24,6 +20,8 @@ public class FetchNodeDetails {
     public static String PROXY_ADDRESS_AQUA = "0x29Dea82a0509153b91040ee13cDBba0f03efb625";
     public static String PROXY_ADDRESS_CELESTE = "0x6Bffb4e89453069E7487f0fa5c9f4a2D771cce6c";
 
+    private String fndServerEndpoint = "https://fnd.tor.us/node-details";
+
 
     public static HashMap<TorusNetwork, String> NETWORK_MAP = new HashMap<TorusNetwork, String>() {
         {
@@ -35,7 +33,6 @@ public class FetchNodeDetails {
         }
     };
 
-    public static NodeDetails NODE_DETAILS_MAINNET = new NodeDetails("19", "0xf20336e16B5182637f09821c27BDe29b0AFcfe80", new String[]{"https://torus-19.torusnode.com/jrpc", "https://torus-node.ens.domains/jrpc", "https://torus-node.matic.network/jrpc", "https://torus.zilliqa.network/jrpc", "https://torus-mainnet.cosmos.network/jrpc", "https://torus2.etherscan.com/jrpc", "https://torus-node-v2.skalelabs.com/jrpc", "https://torus-node.binancex.dev/jrpc", "https://torusnode.ont.io/jrpc"}, new BigInteger[]{new BigInteger("1"), new BigInteger("2"), new BigInteger("3"), new BigInteger("4"), new BigInteger("5"), new BigInteger("6"), new BigInteger("7"), new BigInteger("8"), new BigInteger("9"),}, new TorusNodePub[]{new TorusNodePub("bbe83c64177c3775550e6ba6ac2bc059f6847d644c9e4894e42c60d7974d8c2b", "82b49a7caf70def38cdad2740af45c1e4f969650105c5019a29bb18b21a9acb5"), new TorusNodePub("c208cac4ef9a47d386097a9c915b28e9cb89213abee8d26a17198ee261201b0d", "c7db2fe4631109f40833de9dc78d07e35706549ee48fa557b33e4e75e1047873"), new TorusNodePub("ca1766bb426d4ca5582818a0c5439d560ea64f5baa060793ab29dd3d0ceacfe", "d46c1d08c40e1306e1bca328c2287b8268166b11a1ba4b8442ea2ad0c5e32152"), new TorusNodePub("c3934dd2f6f4b3d2e1e398cc501e143c1e1a381b52feb6d1525af34d16253768", "71f5141a5035799099f5ea3e241e66946bc55dc857ac3bd7d6fcdb8dcd3eeeef"), new TorusNodePub("22e66f1929631d00bf026227581597f085fd94fd952fc0dca9f0833398b5c064", "6088b3912e10a1e9d50355a609c10db7d188f16a2e2fd7357e51bf4f6a74f0a1"), new TorusNodePub("9dc9fa410f3ce9eb70df70cdea00a49f2c4cc7a31c08c0dab5f863ed35ff5139", "627a291cb87a75c61da3f65d6818e1e05e360217179817ed27e8c73bca7ec122"), new TorusNodePub("118b9fc07e97b096d899b9f6658463ce6a8caa64038e37fc969df4e6023dd8c6", "baf9fa4e51770f4796ea165dd03a769b8606681a38954a0a92c4cbffd6609ce9"), new TorusNodePub("8a6d8b925da15a273dec3d8f8395ec35cd6878f274b2b180e4e106999db64043", "96f67f870c157743da0b1eb84d89bf30500d74dc84c11f501ee1cb013acc8c46"), new TorusNodePub("39cecb62e863729f572f7dfc46c24867981bf04bb405fed0df39e33984bfade5", "61c2364434012e68a2be2e9952805037e52629d7762fafc8e10e9fb5bad8f790")}, false);
 
     private final String proxyAddress;
     private final NodeDetails nodeDetails = new NodeDetails();
@@ -67,29 +64,13 @@ public class FetchNodeDetails {
         this.torusLookup.getNodeSet(verifier, hashedVerifierId).sendAsync().whenCompleteAsync((nodeEndPoints, err) -> {
             if (err != null) {
                 if (this.proxyAddress.equals(FetchNodeDetails.PROXY_ADDRESS_MAINNET)) {
-                    cf.complete(FetchNodeDetails.NODE_DETAILS_MAINNET);
+                    //cf.complete(FetchNodeDetails.NODE_DETAILS_MAINNET);
                 } else {
                     cf.completeExceptionally(err);
                 }
                 return;
             }
             try {
-                String[] updatedEndpoints = new String[nodeEndPoints.component3().size()];
-                TorusNodePub[] updatedNodePub = new TorusNodePub[nodeEndPoints.component3().size()];
-
-                for (int i = 0; i < nodeEndPoints.component3().size(); i++) {
-                    NodeInfo endPointElement = new NodeInfo(String.valueOf(nodeEndPoints.component2().get(i)), new BigInteger(String.valueOf(nodeEndPoints.component3().get(i)), 10).toString(16).replace("0x", ""), new BigInteger(String.valueOf(nodeEndPoints.component4().get(i)), 10).toString(16).replace("0x", ""));
-
-                    String endpoint = "https://" + endPointElement.getDeclaredIp().split(":")[0] + "/jrpc";
-                    updatedEndpoints[i] = endpoint;
-                    updatedNodePub[i] = new TorusNodePub(endPointElement.getPubKx(), endPointElement.getPubKy());
-                }
-                this.nodeDetails.setNodeListAddress(this.proxyAddress);
-                this.nodeDetails.setCurrentEpoch(nodeEndPoints.component1().toString());
-                this.nodeDetails.setTorusNodeEndpoints(updatedEndpoints);
-                this.nodeDetails.setTorusNodePub(updatedNodePub);
-                this.nodeDetails.setTorusIndexes(nodeEndPoints.component5().toArray(new BigInteger[0]));
-                this.nodeDetails.setUpdated(true);
                 cf.complete(this.nodeDetails);
             } catch (Exception err2) {
                 cf.completeExceptionally(err2);
